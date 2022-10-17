d4e67878d3ed609411cb22f716f88383555c4373
Ton-10 <taveon30@yahoo.com>
2022-10-16 20:57:28 -0500

    Update Card 050 to work with players and animate properly

--- Assets/Scripts/Cards/Card050.cs
+++ Assets/Scripts/Cards/Card050.cs
@@ -7,8 +7,11 @@ public class Card050 : Card
 {
     static readonly string cardPrefabPath = "Card";
     static readonly string Indicator = "Indicator";
+    private float speed = 60f;
     List<DamageS> scripts = new List<DamageS>();
     GameObject projecile;
+    float indicatorPlacementDistance = 0.34f;
+
     override public bool Hit
     {
         get {
@@ -19,8 +22,18 @@ public class Card050 : Card
             if (projecile != null)
             {
                 GameObject target = scripts[0].hits[scripts[0].hits.Count-1].gameObject;
+                target.GetComponent<CombatMovement>().WaitForTime(0.5f);
+                GameObject shp = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                shp.GetComponent<SphereCollider>().isTrigger = true;
+                shp.transform.position = projecile.transform.position;
+                for (int i = 0; i < 30; i++)
+                {
+                    shp.transform.localScale = shp.transform.localScale + new Vector3(i/100, i/100, i/100);
+
+                }
                 Destroy(projecile);
-                target.GetComponent<CombatMovement>().CanMove = false;
+                Destroy(shp,3f);
+
             }
 
         }
@@ -30,7 +43,8 @@ public class Card050 : Card
     {
         Debug.Log(int.Parse(GetType().ToString().Split('d')[1]));
         data = new CardData(40, 3, 1, int.Parse(GetType().ToString().Split('d')[1]), 5);
-        
+
+
     }
     public void OnEnable()
     {
@@ -38,6 +52,7 @@ public class Card050 : Card
         data.CardObj.GetComponent<Linker>().cardScript = this;
         data.CardObj.gameObject.transform.Find("Attack").GetComponent<Text>().text = data.Attack.ToString();
         data.Indicator = Instantiate(Resources.Load(Indicator) as GameObject);
+        data.Indicator.transform.SetParent(data.CardObj.transform);
         data.CardObj.GetComponent<Image>().color = new Color(1f, 0.6f, 0.6f);
     }
     public override IEnumerator Activate()
@@ -47,22 +62,26 @@ public class Card050 : Card
         {
             data.Player.GetComponent<CombatMovement>().WaitForTime(data.CooldownFrames / 60f);
         }
-        //data.Player.transform.Find("Body").gameObject.GetComponent<Animator>().SetTrigger("Shoot");
-        
-        float indicatorPlacementDistance = 0.34f;
-        if(data.Player.CompareTag("NPC")){
+
+        data.Player.GetComponent<MovementController>().anim.SetTrigger("Shoot");
+
+        if (data.Player.CompareTag("NPC"))
+        {
             indicatorPlacementDistance = -0.34f;
+            speed = -60f;
         }
 
         //start and shoot projectile
         projecile = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        projecile.GetComponent<BoxCollider>().isTrigger = true;
         projecile.transform.position = data.Player.transform.position;
-        projecile.transform.SetParent(data.Player.transform);
+        projecile.GetComponent<BoxCollider>().isTrigger = true;
+        projecile.transform.SetParent(data.IndicatorField.transform);
+        projecile.transform.localPosition = 
+            projecile.transform.localPosition + new Vector3(0, 0, indicatorPlacementDistance*1);
         Rigidbody rb = projecile.AddComponent<Rigidbody>();
         rb.useGravity = false;
-        rb.velocity = new Vector3(0, 0, -1) * 60;
-        Destroy(projecile, 1.5f);
+        rb.velocity = new Vector3(0, 0, 1f) * speed;
+        Destroy(projecile, 0.32f);
 
         for (int i = 1; i < data.RangeX + 1; i++)
         {
@@ -73,22 +92,20 @@ public class Card050 : Card
             data.CurIndicator.GetComponent<DamageS>().creator = this;
             data.CurIndicator.GetComponent<DamageS>().Damage = data.Attack;
             data.CurIndicator.transform.localPosition = 
-                new Vector3(
-                    data.CurIndicator.transform.localPosition.x, 
-                    data.CurIndicator.transform.localPosition.y, 
-                    data.CurIndicator.transform.localPosition.z + indicatorPlacementDistance * i);
+                data.CurIndicator.transform.localPosition + new Vector3(0, 0, indicatorPlacementDistance * i);
             data.CurIndicator.transform.localScale = new Vector3(0.04f, 0.04f, 1);
+
             Destroy(data.CurIndicator, 0.15f);
             // Sync damage instances
             foreach (DamageS script in scripts)
             {
                 script.DamageInstances = scripts;
             }
-            yield return new WaitForSeconds(0.5f / data.RangeX);
+            yield return new WaitForSeconds(0.15f);
         }
         if (data.Player.CompareTag("NPC"))
         {
-            data.Player.GetComponent<EnemyController>().CanMove = true;
+            data.Player.GetComponent<MovementController>().CanMove = true;
         }
     }
 }
