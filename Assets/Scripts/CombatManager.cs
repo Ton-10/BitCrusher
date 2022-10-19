9629dc5419b62dd0f2020983758d1f6a27dae8ae
Ton-10 <taveon30@yahoo.com>
2022-10-18 22:56:18 -0500

    Update combat loop (loop complete) to correctly position camera after fight

--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 public class CombatManager : MonoBehaviour
 {
     public bool InCombat;
-    Vector3 originalPos;
+    Vector3 originalPlayerPos, originalCamPos;
+    Quaternion originalCamRot;
     GameObject enemies = null;
     public void StartCombat(GameObject enemy)
     {
         InCombat = true;
         enemies = enemy;
-        originalPos = gameObject.transform.Find("Body").transform.position;
+        originalPlayerPos = gameObject.transform.Find("Body").transform.position;
         gameObject.GetComponent<MapMovement>().CanMove = false;
         gameObject.GetComponent<CombatMovement>().CanMove = true;
         gameObject.GetComponent<CombatMovement>().enabled = true;
         GameObject Cam = GameObject.FindGameObjectWithTag("MainCamera");
+        originalCamPos = Cam.transform.position;
+        originalCamRot = Cam.transform.rotation;
         Cam.transform.rotation = Quaternion.Euler(0, -90, 0);
         Cam.transform.position = gameObject.GetComponent<CombatMovement>().PlayerField.transform.Find("Center").position + new Vector3(50, 0, 0);
         gameObject.GetComponent<Custom>().enabled = true;
@@ -28,13 +31,15 @@ public class CombatManager : MonoBehaviour
         {
             if (InCombat == true && (!gameObject.GetComponent<Hp>().Alive || !enemies.GetComponent<Hp>().Alive))
             {
+                // Combat end
                 gameObject.GetComponent<MapMovement>().CanMove = true;
                 gameObject.GetComponent<CombatMovement>().CanMove = false;
                 gameObject.GetComponent<CombatMovement>().enabled = false;
-                gameObject.transform.Find("Body").transform.position = originalPos;
+                gameObject.transform.Find("Body").transform.position = originalPlayerPos;
                 GameObject Cam = GameObject.FindGameObjectWithTag("MainCamera");
-                Cam.transform.rotation = Quaternion.Euler(45, -90, 0);
-                Cam.transform.position = gameObject.transform.Find("Body").transform.position + new Vector3(100, 0, 0);
+                Cam.transform.position = originalCamPos;
+                Cam.transform.rotation = originalCamRot;
+                
                 Destroy(enemies);
             }
         }
